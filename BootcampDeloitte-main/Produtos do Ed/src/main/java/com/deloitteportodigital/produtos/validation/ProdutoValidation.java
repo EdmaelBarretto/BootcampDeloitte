9b1c5d8fb8a6ddd8cbcc01395a3cb6687939e456
Edmael Barretto <edmael_barreto@hotmail.com>
2026-03-19 20:13:13 -0300

    Delete BootcampDeloitte-main directory

--- BootcampDeloitte-main/Produtos do Ed/src/main/java/com/deloitteportodigital/produtos/validation/ProdutoValidation.java
+++
@@ -1,9 +0,0 @@
-package com.deloitteportodigital.produtos.validation;
-
-import com.deloitteportodigital.produtos.model.Produto;
-
-public interface ProdutoValidation {
-
-    void validar(Produto produto);
-
-}
